3e4d4c8d66fda9ced167bc0a357cd9011ec0e8cf
Mattis Erkensten <m.erkensten@icloud.com>
2022-01-11 11:31:34 +0100

    Simpel style för inputs skapad och form tillagd i login vyn

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -1,7 +1,17 @@
+// Styles 
+import styles from './Login.module.css'
+
 export const Login = () => {
   return (
-    <>
-      <h1> Login Page </h1>
-    </>
+    <div className={styles.login_container}>
+      <div className={styles.login_wrapper}>
+        <h2>Sign In to E-dice</h2>
+        <form className={styles.login_form}>
+          <input type="text" name="username" placeholder="Type in your username"/>
+          <input type="password" name="password" placeholder="Type in your password"/>
+          <input type="button" value="Sign In" />
+        </form>
+      </div>
+    </div>
   );
 };

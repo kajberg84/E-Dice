ce9444d7614230a496a433828f6bfb8b8e2975dc
Tinctoria <quizzie_89@hotmail.com>
2022-01-12 20:28:20 +0100

    add getUser function to api

--- src/api/users.js
+++ src/api/users.js
@@ -1,45 +1,55 @@
-export const users = [user, user2, user3];
+export const users = [
+  {
+    id: 1,
+    fname: "Ida",
+    lname: "Idasson",
+    adress: "Idavägen 2",
+    postnr: 11111,
+    city: "Idastad",
+    phone: "070-0000000",
+    email: "Ida@Ida.com",
+    userlevel: "basic",
+    password: "Ida1",
+    created: "2022-01-12",
+  },
+  {
+    id: 2,
+    fname: "Kaj",
+    lname: "Kajson",
+    adress: "Kajvägen 2",
+    postnr: 11111,
+    city: "Kajstad",
+    phone: "070-0000001",
+    email: "Kaj@Kaj.com",
+    userlevel: "basic",
+    password: "Kaj1",
+    created: "2022-01-12",
+  },
+  {
+    id: 3,
+    fname: "Mattis",
+    lname: "Mattisson",
+    adress: "Mattisvägen 2",
+    postnr: 11111,
+    city: "Mattisstad",
+    phone: "070-0000002",
+    email: "Mattis@Mattis.com",
+    userlevel: "basic",
+    password: "Mattis1",
+    created: "2022-01-12",
+  },
+];
 
-const user = {
-  id: 1,
-  fname: "Ida",
-  lname: "Idasson",
-  adress: "Idavägen 2",
-  postnr: 11111,
-  city: "Idastad",
-  phone: "070-0000000",
-  email: "Ida@Ida.com",
-  password: "Ida1",
-  userlevel: "basic",
-  created: "2022-01-12",
-};
-
-const user2 = {
-  id: 2,
-  fname: "Kaj",
-  lname: "Kajson",
-  adress: "Kajvägen 2",
-  postnr: 11111,
-  city: "Kajstad",
-  phone: "070-0000001",
-  email: "Kaj@Kaj.com",
-  password: "Kaj1",
-  userlevel: "basic",
-  created: "2022-01-12",
-};
-
-const user3 = {
-  id: 2,
-  fname: "Mattis",
-  lname: "Mattisson",
-  adress: "Mattisvägen 2",
-  postnr: 11111,
-  city: "Mattisstad",
-  phone: "070-0000002",
-  email: "Mattis@Mattis.com",
-  password: "Mattis1",
-  userlevel: "basic",
-  created: "2022-01-12",
+const getUser = (email, password) => {
+  const user = users.filter(
+    (user) => user.email === email && user.password === password
+  );
+  if (user) {
+    const { password, created, ...userInfo } = user;
+    return userInfo;
+  } else {
+    return {};
+  }
 };
 
 // getuser(email, password)

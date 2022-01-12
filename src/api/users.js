7a73f5445ef3d43b8487fdf459f9d085e398b360
Kaj Berg <kb223aw@student.lnu.se>
2022-01-12 16:12:29 +0100

    refactor user api

--- src/api/users.js
+++ src/api/users.js
@@ -1,65 +1,44 @@
-export const users = [user, user2, user3];
-
-const user = {
-  id: 1,
-  fname: "Ida",
-  lname: "Idasson",
-  adress: "Idavägen 2",
-  postnr: 11111,
-  city: "Idastad",
-  phone: "070-0000000",
-  email: "Ida@Ida.com",
-  password: "Ida1",
-  userlevel: "basic",
-  created: "2022-01-12",
-};
-
-const user2 = {
-  id: 2,
-  fname: "Kaj",
-  lname: "Kajson",
-  adress: "Kajvägen 2",
-  postnr: 11111,
-  city: "Kajstad",
-  phone: "070-0000001",
-  email: "Kaj@Kaj.com",
-  password: "Kaj1",
-  userlevel: "basic",
-  created: "2022-01-12",
-};
-
-const user3 = {
-  id: 2,
-  fname: "Mattis",
-  lname: "Mattisson",
-  adress: "Mattisvägen 2",
-  postnr: 11111,
-  city: "Mattisstad",
-  phone: "070-0000002",
-  email: "Mattis@Mattis.com",
-  password: "Mattis1",
-  userlevel: "basic",
-  created: "2022-01-12",
-};
+export const users = [
+  {
+    id: 1,
+    fname: "Ida",
+    lname: "Idasson",
+    adress: "Idavägen 2",
+    postnr: 11111,
+    city: "Idastad",
+    phone: "070-0000000",
+    email: "Ida@Ida.com",
+    password: "Ida1",
+    userlevel: "basic",
+    created: "2022-01-12",
+  },
+  {
+    id: 2,
+    fname: "Kaj",
+    lname: "Kajson",
+    adress: "Kajvägen 2",
+    postnr: 11111,
+    city: "Kajstad",
+    phone: "070-0000001",
+    email: "Kaj@Kaj.com",
+    password: "Kaj1",
+    userlevel: "basic",
+    created: "2022-01-12",
+  },
+  {
+    id: 2,
+    fname: "Mattis",
+    lname: "Mattisson",
+    adress: "Mattisvägen 2",
+    postnr: 11111,
+    city: "Mattisstad",
+    phone: "070-0000002",
+    email: "Mattis@Mattis.com",
+    password: "Mattis1",
+    userlevel: "basic",
+    created: "2022-01-12",
+  },
+];
 
 // getuser(email, password)
 // kolla mot voran egna users att denna finns och returnera ett objekt.
-
-/*
-user = {
-  id: xxx,
-  fname: xxx,
-  lname: xxx,
-  adress: xxx,
-  postnr: xxx,
-  city: xxx,
-  phone: xxx,
-  email: xxx,
-  password: xxx,
-  userlevel: xxx,
-  created: xxx,
-  
-}
-
-
-*/

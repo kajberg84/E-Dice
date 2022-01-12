b4291b340ac58d0fbe4a4834e41191550f76f203
Kaj Berg <kb223aw@student.lnu.se>
2022-01-12 08:20:45 +0100

    Added useffect/states

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,19 +1,42 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-export const UserContext = createContext();
+export const UserContext = createContext(null);
 
 const UserProvider = ({ children }) => {
-  const [loggedInUser, setLoggedInUser] = useState(false);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
+
+  // Usertoken state. undefined if not in localstorage
+  const [userToken, setUserToken] = useState(() => {
+    if (typeof window !== "undefined") {
+      if (window.localStorage.getItem("userToken")) {
+        const storageToken = window.localStorage.getItem("userToken");
+        return JSON.parse(storageToken);
+      }
+      return "";
+    }
+  });
+
+  // setting userToken to LocalStorage
+  function setUserTokenLocalStorage(token) {
+    if (typeof window !== "undefined" && token !== "") {
+      window.localStorage.setItem("userToken", JSON.stringify(token));
+      setUser(token);
+    } else {
+      window.localStorage.removeItem("userTokens");
+    }
+  }
+
+  // Updating usertoken if changed/refreshed
+  useEffect(() => {
+    setUserTokenLocalStorage(userToken);
+  }, [userToken]);
 
   return (
     <>
       <UserContext.Provider
         value={{
-          loggedInUser,
-          setLoggedInUser,
-          user,
-          setUser,
+          userValue: [user, setUser],
+          tokenValue: [userToken, setUserToken],
         }}
       >
         {children}

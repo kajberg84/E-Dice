d810776ca50e32a318e5a9ce9801dc3e9f6af49d
Kaj Berg <kb223aw@student.lnu.se>
2022-01-12 08:38:32 +0100

    Refactor Form

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -1,15 +1,48 @@
-// Styles 
-import styles from './Login.module.css'
+//Imports
+import { useState, useContext } from "react";
+// import { UserContext } from "../../context/UserContext";
+// Styles
+import styles from "./Login.module.css";
 
 export const Login = () => {
+  // const [userValue, tokenValue] = useContext(UserContext);
+  // const [user, setUser] = userValue;
+  // const [userToken, setUserToken] = tokenValue;
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  //Handling login
+  function handleLogin(e) {
+    e.preventDefault();
+    console.log("submit form", userEmail, userPassword);
+  }
+
   return (
     <div className={styles.login_container}>
       <div className={styles.login_wrapper}>
         <h2>Sign In to E-dice</h2>
-        <form className={styles.login_form}>
-          <input type="text" name="email" placeholder="Type in your email"/>
-          <input type="password" name="password" placeholder="Type in your password"/>
-          <input type="button" value="Sign In" />
+        <form onSubmit={handleLogin} className={styles.login_form} noValidate>
+          <input
+            type='email'
+            name='email'
+            placeholder='Type in your email'
+            required
+            onChange={(e) => {
+              setUserEmail(e.target.value);
+            }}
+          />
+          <input
+            type='password'
+            name='password'
+            placeholder='Type in your password'
+            required
+            onChange={(e) => {
+              setUserPassword(e.target.value);
+            }}
+          />
+          <button type='submit' className={`${styles.loginbutton} inputButton`}>
+            Sign in
+          </button>
         </form>
       </div>
     </div>

f41d959745d55a9e68f9240f17a4cf1892a8fb35
Kaj Berg <kb223aw@student.lnu.se>
2022-01-13 13:34:04 +0100

    add Customlink sfc

---
+++ src/utils/CustomLink.jsx
@@ -0,0 +1,20 @@
+import { useMatch, useResolvedPath, Link } from "react-router-dom";
+
+function CustomLink({ children, to, ...props }) {
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+    <div>
+      <Link
+        style={{ textDecoration: match ? "underline" : "none" }}
+        to={to}
+        {...props}
+      >
+        {children}
+      </Link>
+    </div>
+  );
+}
+
+export default CustomLink;

a303460b2d5ad105afeee66c64cf72e2d7cb68b5
Kaj Berg <kb223aw@student.lnu.se>
2022-01-14 21:38:15 +0100

    Add LoginForm validation

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -5,63 +5,70 @@ import { getUser } from "../../api/users";
 import { setLocalStorage } from "../../utils/localStorageHandler";
 import { useNavigate } from "react-router-dom";
 import { RoutingPath } from '../../routes/RoutingPath';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 // Styles
 import styles from "./Login.module.css";
+
+// Schema for formvalidating
+const loginSchema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  })
+  .required();
 
 export const Login = () => {
   let navigate = useNavigate();
   const { user, setUser } = useContext(UserContext);
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(loginSchema),
+  });
 
-  //Handling login
-  async function handleLogin(e) {
-    e.preventDefault();
-
-    //validate input ( fixa en bättre lösning )
-    if (userEmail === "" || userPassword === "") {
-      alert("Please enter your email and password");
-      return;
-    }
-
+  // On submit
+  const onSubmit = async (data) => {
+    console.log(data);
     try {
       const responseUser = await getUser(userEmail, userPassword);
+
       if (!responseUser) {
-        console.log("Not correct validation or no user", responseUser);
-        alert("Wrong email/ password or no user");
+        alert("Wrong email/ password or no user found");
         return;
       }
 
-      console.log("user: ", responseUser);
       setLocalStorage("edice-user", responseUser);
       setUser(responseUser);
     } catch (error) {
       console.log("Login: ", error);
     }
 
-    navigate(`/${RoutingPath.Account}`)
-    // navigate("/account")
-  }
+    navigate(`/${RoutingPath.Account}`);
+  };
 
   return (
     <div className={styles.login_container}>
       <div className={styles.login_wrapper}>
         <h2>Sign In to E-dice</h2>
-        <form onSubmit={handleLogin} className={styles.login_form} noValidate>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.login_form}>
           <input
-            type='email'
-            name='email'
+            {...register("email")}
             placeholder='Type in your email'
-            required
             onChange={(e) => {
               setUserEmail(e.target.value);
             }}
           />
+          <p>{errors.email?.message}</p>
           <input
-            type='password'
-            name='password'
+            {...register("password")}
             placeholder='Type in your password'
-            required
             onChange={(e) => {
               setUserPassword(e.target.value);
             }}
